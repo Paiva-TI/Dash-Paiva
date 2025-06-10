caa4759aa1ccdc30175842aab336c9c6bca14cd0
Paiva-TI <suporte@paivaenergia.com.br>
2025-06-10 17:59:57 -0300

    att: página sign up

--- frontend/app/auth/sign-up-success/page.tsx
+++ frontend/app/auth/sign-up-success/page.tsx
@@ -13,13 +13,13 @@ export default function Page() {
         <div className="flex flex-col gap-6">
           <Card>
             <CardHeader>
-              <CardTitle className="text-2xl">Thank you for signing up!</CardTitle>
-              <CardDescription>Check your email to confirm</CardDescription>
+              <CardTitle className="text-2xl">Obrigado por se cadastrar!</CardTitle>
+              <CardDescription>Cheque seu email para confirmar</CardDescription>
             </CardHeader>
             <CardContent>
               <p className="text-sm text-muted-foreground">
-                You&apos;ve successfully signed up. Please check your email to confirm your account
-                before signing in.
+                Seu cadastro foi um sucesso. Por favor cheque seu email para confirmar sua conta
+                antes de fazer login.
               </p>
             </CardContent>
           </Card>
