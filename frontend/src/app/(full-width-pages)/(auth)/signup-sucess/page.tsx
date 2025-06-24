a89c966e70f4f4f765a38bf847c9fd4093966c4d
Paiva-TI <suporte@paivaenergia.com.br>
2025-06-24 11:01:54 -0300

    🚨Ajustado erro de importação

--- frontend/src/app/(full-width-pages)/(auth)/signup-sucess/page.tsx
+++ frontend/src/app/(full-width-pages)/(auth)/signup-sucess/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import SignUpSucessForm from "@/components/auth/SignUpSucess"
 
 export const metadata: Metadata = {
   title: "Conta criada | Dashboard Paiva Energia",
@@ -6,5 +7,5 @@ export const metadata: Metadata = {
 };
 
 export default function SignUpSucess() {
-  return <SignUpSucess />;
+  return <SignUpSucessForm />;
 }
--- frontend/src/components/auth/SignUpSucess.tsx
+++ frontend/src/components/auth/SignUpSucess.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 
-export default function SignUpSucess() {
+export default function SignUpSucessForm() {
   return (
     <div className="flex flex-col flex-1 lg:w-1/2 w-full overflow-y-auto no-scrollbar">
       
